7a0106a99e4ad890567b69ff0474d681efd010bd
Sharique Imam <shariquegithubweb@gmail.com>
2023-02-06 01:45:36 +0500

    Updated

--- pages/api/email-to-owner.js
+++ pages/api/email-to-owner.js
@@ -12,7 +12,7 @@ export default function (req, res) {
   // console.log(req.body)
   const mailData = {
     from: "recepientbot1212@gmail.com",
-    to: "shariquei061@gmail.com",
+    to: "v.tasev@sftdx.com",
     subject: `Message From My Mode Ride Bot`,
     text: req.body.Name,
     html: `<div>Hi Owner Name,  <br> ${req.body.Name} has placed a request on the website. <br> <strong> The details are: </strong><br> Name:  ${req.body.Name} <br>  Email:  ${req.body.Email}<br> Option:  ${req.body.Option}<br> Details:  ${req.body.Details}<br>Thanks</div>`,
